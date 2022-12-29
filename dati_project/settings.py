c4f1a4f2a509ea4d56ff7b23222a71bacf6a68ab
LuisBusso <lbusso55@gmail.com>
2022-12-29 20:09:01 -0300

    first commit

--- dati_project/settings.py
+++ dati_project/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = (os.path.join(BASE_DIR, 'code/staticfiles'))
+STATIC_ROOT = (os.path.join(BASE_DIR, 'static'))
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
